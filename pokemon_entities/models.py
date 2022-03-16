52dba1f79b66ddc519229038ea5946303bb0793e
Nurbek Kuspanov <nurbek@inbox.ru>
2022-03-16 17:20:59 +0400

    added relation to Pokemon model

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -10,5 +10,6 @@ class Pokemon(models.Model):
 
 
 class PokemonEntity(models.Model):
+    pokemon = models.ForeignKey(Pokemon, null=True, on_delete=models.CASCADE)
     lat = models.FloatField()
     lon = models.FloatField()
